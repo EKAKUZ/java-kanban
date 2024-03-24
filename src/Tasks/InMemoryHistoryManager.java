40a0e4ade508915a2dc7e45bd539b3439eb84db5
Ekaterina Kuzmina <pronina.katia@yandex.ru>
2024-03-24 22:25:36 +0300

    Создан новый класс, в него перенесена проверка на инициализацию и готовность менеджеров к работе после вызова методов get класса Menegers

--- src/Tasks/InMemoryHistoryManager.java
+++ src/Tasks/InMemoryHistoryManager.java
@@ -20,20 +20,20 @@ public class InMemoryHistoryManager implements HistoryManager{
                 Task newTask = new Task(task.getName(), task.getDescription(), task.getId(), task.getStatus());
                 historyTasks.add(newTask);
             } else if (task.getClass().equals(SubTask.class)) {
-                Task newTask = new SubTask(task.getName(), task.getDescription(), task.getId(), task.getStatus());
-                ((SubTask) newTask).setEpicId(((SubTask) task).getEpicId());
+                SubTask newTask = new SubTask(task.getName(), task.getDescription(), task.getId(), task.getStatus());
+                newTask.setEpicId(((SubTask) task).getEpicId());
                 historyTasks.add(newTask);
             } else if (task.getClass().equals(Epic.class)) {
-                Task newTask = new Epic(task.getName(), task.getDescription(), task.getId());
+                Epic newTask = new Epic(task.getName(), task.getDescription(), task.getId());
                 ArrayList<Integer> ArraySubTaskId = ((Epic) task).getArraySubTaskId();
                 for (Integer id: ArraySubTaskId) {
-                    ((Epic) newTask).addSubTaskId(id);
+                    newTask.addSubTaskId(id);
                 }
                 historyTasks.add(newTask);
             }
 
             if (historyTasks.size() > tasksLimit) {
-                historyTasks.remove(0);
+                historyTasks.removeFirst();
             }
         }
     }
